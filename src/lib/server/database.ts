0b096b9ed55d98968a2335888a98c60827c92a82
Bohumil Jahoda <jahoda@jecas.cz>
2023-01-08 12:05:33 +0100

    Disqus

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,5 +1,5 @@
 import Database from 'better-sqlite3';
 import fs from 'node:fs';
 
-export const db = new Database(fs.readFileSync('mysqlite3.db'));
+export const db = new Database(fs.readFileSync('./mysqlite3.db'));
 db.pragma('journal_mode = WAL');
