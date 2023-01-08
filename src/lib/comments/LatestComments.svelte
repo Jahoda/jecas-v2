61dfa2f123a36dbedfa27cd99e15b9d0980af415
Bohumil Jahoda <jahoda@jecas.cz>
2023-01-08 12:05:33 +0100

    Disqus

--- src/lib/comments/LatestComments.svelte
+++ src/lib/comments/LatestComments.svelte
@@ -6,14 +6,16 @@
 	let comments: CommentContent[] = [];
 
 	async function fetchLatestComments() {
-		const response = await fetch(
-			'https:////disqus.com/api/3.0/forums/listPosts.json?forum=jecas&limit=10&related=thread&api_key=BwcwyR03Y19LVAHRVIq0Uly6e0L0QOjaIlrpEaUSoAu8hnUZ8iKJoNllOXT2bSue'
-		);
-		const data = await response.json();
+		try {
+			const response = await fetch(
+				'https:////disqus.com/api/3.0/forums/listPosts.json?forum=jecas&limit=10&related=thread&api_key=BwcwyR03Y19LVAHRVIq0Uly6e0L0QOjaIlrpEaUSoAu8hnUZ8iKJoNllOXT2bSue'
+			);
+			const data = await response.json();
 
-		if (data.code === 0) {
-			comments = data.response;
-		}
+			if (data.code === 0) {
+				comments = data.response;
+			}
+		} catch (_e) {}
 	}
 	onMount(fetchLatestComments);
 </script>
