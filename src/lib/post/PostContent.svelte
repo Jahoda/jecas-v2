df13bc77e9ea1575a7ee673762bf8bafb1257b9b
Bohumil Jahoda <jahoda@jecas.cz>
2025-08-27 08:34:53 +0200

    kbd style

--- src/lib/post/PostContent.svelte
+++ src/lib/post/PostContent.svelte
@@ -44,7 +44,9 @@
 <div
 	class="
 		prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden prose-a:text-blue-dark prose-a:hover:no-underline prose-blockquote:bg-slate-200 dark:prose-blockquote:bg-slate-700 prose-blockquote:p-4 prose-blockquote:rounded-md
-		prose-blockquote:border-l-4 prose-blockquote:border-slate-300 dark:prose-blockquote:border-slate-600 m-auto w-full max-w-3xl
+		prose-blockquote:border-l-4 prose-blockquote:border-slate-300 dark:prose-blockquote:border-slate-600
+		prose-kbd:bg-gray-100 dark:prose-kbd:bg-gray-800 prose-kbd:border prose-kbd:border-gray-300 dark:prose-kbd:border-gray-600 prose-kbd:rounded prose-kbd:px-1.5 prose-kbd:py-0.5 prose-kbd:text-xs prose-kbd:align-middle prose-kbd:font-mono prose-kbd:shadow-sm prose-kbd:text-gray-800 dark:prose-kbd:text-gray-200
+		m-auto w-full max-w-3xl
 		"
 	bind:this={postContent}
 >
