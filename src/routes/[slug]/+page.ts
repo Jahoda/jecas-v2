187736be44d6497df2ac95c7c5adaa870c7d0caa
Bohumil Jahoda <jahoda@jecas.cz>
2025-12-15 09:06:44 +0100

    Oprava pre-renderingu kontaktní stránky (#36)

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -5,14 +5,16 @@ export const prerender = true;
 
 // Generate entries for all posts and tags at build time
 export const entries: EntryGenerator = async () => {
-	const { getAllPosts } = await import('$lib/post/post');
+	const { getAllPosts, getAllDrafts } = await import('$lib/post/post');
 	const { getAllUsedTags } = await import('$lib/tag/tags');
 
 	const posts = await getAllPosts();
+	const drafts = await getAllDrafts();
 	const tags = await getAllUsedTags();
 
 	const postEntries = posts.map((post) => ({ slug: post.url_slug }));
+	const draftEntries = drafts.map((draft) => ({ slug: draft.url_slug }));
 	const tagEntries = tags.map((tag) => ({ slug: tag.url_slug }));
 
-	return [...postEntries, ...tagEntries];
+	return [...postEntries, ...draftEntries, ...tagEntries];
 };
